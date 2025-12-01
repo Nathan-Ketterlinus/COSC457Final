a1e9890332220145cbdb021a4446722550134006
Hales124 <helliot1@students.towson.edu>
2025-11-30 21:22:59 -0500

    All Displays Working

---
+++ SQL script/testDropTables.sql
@@ -0,0 +1,15 @@
+use narella1db;
+
+DROP TABLE testWorkerCreates;
+
+DROP TABLE testFavor;
+
+DROP TABLE testInventory;
+
+DROP TABLE testEvent;
+
+DROP TABLE testContractor;
+
+DROP TABLE testClient;
+
+DROP TABLE testWorker;
